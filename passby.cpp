9cdedfbe057b0e56dcf8116b38f284d9a2c12896
Prasiddapro <officialprasidda@gmail.com>
2026-06-26 17:41:21 +0100

    first commit

--- passby.cpp
+++
@@ -1,2 +0,0 @@
-Pass by reference means: instead of sending a copy of a variable’s value to a function,
- we send the memory address of the original variable.
--- swap2.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-void sumAndProduct(int &a, int &b) {
-    int sum = a + b
-    int product = a * b;
-
